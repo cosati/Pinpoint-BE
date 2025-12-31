9fc049b7acf139d5754f0d67784f867b99e1ccc3
cosati <cosati.alexandre@gmail.com>
2025-12-31 14:59:31 +0100

    Disable full context test

--- src/test/java/com/cosati/photo_map/PhotoMapApplicationTests.java
+++ src/test/java/com/cosati/photo_map/PhotoMapApplicationTests.java
@@ -3,7 +3,7 @@ package com.cosati.photo_map;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class PhotoMapApplicationTests {
 
 	@Test
