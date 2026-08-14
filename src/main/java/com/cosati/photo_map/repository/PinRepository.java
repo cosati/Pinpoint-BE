3fc25d39361695a9cf340d3977df2b36ebc38e0e
cosati <cosati.alexandre@gmail.com>
2026-08-14 21:07:33 +0200

    Update repositories

--- src/main/java/com/cosati/photo_map/repository/PinRepository.java
+++ src/main/java/com/cosati/photo_map/repository/PinRepository.java
@@ -1,9 +1,10 @@
 package com.cosati.photo_map.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.cosati.photo_map.domain.Pin;
 
-public interface PinRepository extends JpaRepository<Pin, Long> {
+public interface PinRepository extends JpaRepository<Pin, UUID> {
   Optional<Pin> findByFileName(String fileName);
 }
