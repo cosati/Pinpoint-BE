fbf21c4392432c82dd16c1ad04dacaac41248692
cosati <cosati.alexandre@gmail.com>
2026-08-14 20:08:16 +0200

    UserRepository

---
+++ src/main/java/com/cosati/photo_map/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cosati.photo_map.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cosati.photo_map.domain.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByEmail(String email);
+}
