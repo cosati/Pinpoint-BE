83b0a4310139963d3e2238d3f6fd3e140af88173
cosati <cosati.alexandre@gmail.com>
2026-08-15 15:20:37 +0200

    Add serialVersionUID to exceptions

--- src/main/java/com/cosati/photo_map/exceptions/EmailAlreadyRegisteredException.java
+++ src/main/java/com/cosati/photo_map/exceptions/EmailAlreadyRegisteredException.java
@@ -1,6 +1,8 @@
 package com.cosati.photo_map.exceptions;
 
 public class EmailAlreadyRegisteredException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+  
   public EmailAlreadyRegisteredException(String message) {
     super(message);
   }
--- src/main/java/com/cosati/photo_map/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/cosati/photo_map/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,8 @@
 package com.cosati.photo_map.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
   public ResourceNotFoundException(String message) {
     super(message);
   }
